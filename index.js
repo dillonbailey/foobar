0545ee856de09aaa449b1d62f68fec3e91502dbb
Dillon Bailey <dillon@honestfox.com.au>
2017-12-10 00:47:14 +1100

    init release

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+// var MongoClient = require("mongodb").MongoClient;
+
+// MongoClient.connect(process.env.MONGO_URI, function(err, db) {
+//   if (err) {
+//     console.warn("Cannot connect to MongoDB!", err);
+//   } else {
+//     console.log("Connected to mongoDB");
+//   }
+// });
+
+const path = require("path");
+const express = require("express");
+const proxy = require("express-http-proxy");
+const baseImageUrl = process.env.BASE_IMAGE_URL;
+const proxyBaseImageUrl = baseImageUrl
+  ? proxy(baseImageUrl, {
+      proxyReqPathResolver: function(req) {
+        const newPath = baseImageUrl + req.path;
+        console.log(newPath);
+        return newPath;
+      }
+    })
+  : express.static(path.join(__dirname, "public/images"));
+const app = express();
+
+app.use("/images", proxyBaseImageUrl);
+
+app.listen(8080);
